e328ba728a8d30a93b54bbb7d93a6bb7ac135f3b
Paul Butler III <paul@heysparkbox.com>
2021-06-14 12:53:20 -0600

    feat: utilizing updated components/store

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-    </div>
+    </nav> -->
     <router-view/>
+    <side-panel v-if="activePokemon" :pokemon="activePokemon"></side-panel>
   </div>
 </template>
 
+<script>
+import { mapGetters } from 'vuex';
+import SidePanel from '@/components/SidePanel';
+
+export default {
+  components: {
+    SidePanel,
+  },
+  computed: {
+    ...mapGetters({
+      activePokemon: 'getActivePokemon',
+    })
+  }
+}
+</script>
+
 <style lang="scss">
+$green: #c4e2c7;
+
+body {
+  margin: 0;
+}
+
 #app {
+  background-color: $green;
+  color: #2c3e50;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  padding: 1rem;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,35 @@
 <template>
   <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <pokemon-list :pokemonList="pokemonList"></pokemon-list>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
+import { mapActions, mapGetters } from 'vuex';
+import PokemonList from '@/components/PokemonList';
 
 export default {
   name: 'Home',
-  components: {}
+  components: {
+    PokemonList,
+  },
+  computed: {
+    ...mapGetters({
+      pokemonList: 'getPokemon',
+    }),
+  },
+  created() {
+    this.setPokemon();
+  },
+  methods: {
+    ...mapActions(['setPokemon']),
+  },
 }
 </script>
+
+<style scoped>
+.home {
+  position: relative;
+}
+</style>
