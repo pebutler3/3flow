ed4241b4e8da8cbdd9036bc67f20a4b7e1e3452c
Paul Butler III <paul@heysparkbox.com>
2021-06-14 12:52:44 -0600

    feat: ViewPokemonButton for setting active Pokemon

---
+++ src/components/ViewPokemonButton.vue
@@ -0,0 +1,35 @@
+<template>
+  <button @click="setActivePokemon(url)" :disabled="isDisabled" :aria-disabled="isDisabled">View Pokemon</button>
+</template>
+
+<script>
+import { mapActions } from 'vuex';
+
+export default {
+  props: {
+    url: {
+      type: String,
+      default: '',
+      required: true,
+    },
+    isDisabled: {
+      type: Boolean,
+      default: false,
+      required: false,
+    },
+  },
+  methods: {
+    ...mapActions(['setActivePokemon']),
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+button {
+  padding: 0.5rem 1rem;
+
+  &:not(:disabled) {
+    cursor: pointer;
+  }
+}
+</style>
